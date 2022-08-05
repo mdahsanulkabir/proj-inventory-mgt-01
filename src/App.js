73ede114720bb007cb513127f76e1fdb19bc3f22
ahsan <makabir98@yahoo.com>
2022-08-05 18:20:09 +0600

    google sign in tested and OK

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import "./App.css";
+import auth from "./firebase.init";
 
 function App() {
+  const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+      signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   return (
     <div className="App">
-      
+      <button onClick={handleGoogleSignIn}>Google Sign in</button>
     </div>
   );
 }
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -15,5 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export default app;
+export default auth;
